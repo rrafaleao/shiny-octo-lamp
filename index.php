c46b940a7ed57593239815bed8520e3caab8e545
rafael <162385496+rrafaleao@users.noreply.github.com>
2025-03-12 00:11:58 +0000

    feito

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+            display: flex;
+            margin:0;
+        }
+        .sidebar{
+            width: 250px;
+            background: #333;
+            color: #fefefe;
+            height: 100vh;
+            padding: 20px;
+        }
+        .sidebar ul{
+            list-style: none;
+            padding: 0px;
+        }
+        .sidebar ul li{
+            padding:10px;
+        }
+        .sidebar ul li a{
+            color: #fefefe;
+            text-decoration: none;
+        }
+        .content{
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div class="sidebar">
+        <h2> Dashboard </h2>
+        <ul>
+            <li><a href="#"> inicio</a></li>
+            <li><a href="#"> Relatorios</a></li>
+            <li><a href="#"> Configurações</a></li>
+        </ul>
+    </div>
+    <div class="content">
+        <h2>bem vindo</h2>
+        <p> adsd </p>
+    </div>
+</body>
+</html>
